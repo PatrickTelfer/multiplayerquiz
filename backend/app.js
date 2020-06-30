8aedd1623b14b9c930e66f7b4039e740ca774114
PatrickTelfer <patrickwktelfer@gmail.com>
2020-06-30 15:20:12 -0400

    try different dist path

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var distDir = __dirname + "/dist";
+var distDir = __dirname + "/../dist";
 app.use(express.static(distDir));
 
 app.use((req, res, next) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin', 'http://localhost:4200'
-    );
+    // res.setHeader(
+    //     'Access-Control-Allow-Origin', 'http://localhost:4200'
+    // );
     res.setHeader(
         'Access-Control-Allow-Credentials', 'true'
     )
